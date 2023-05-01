805dd5893a68613cb74519edd33778a7f418e90e
Hakovo66 <113375465+Hakovo66@users.noreply.github.com>
2023-04-30 23:08:35 -0600

    Create Ejercicio-While.java

---
+++ Ejercicio-While.java
@@ -0,0 +1,18 @@
+public class EjercicioWhile {
+
+    public static void main(String[] args) {
+
+        int numeroWhile = 0;
+
+        while (numeroWhile <= 3){
+
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        }
+
+
+
+
+
+    }
+}
